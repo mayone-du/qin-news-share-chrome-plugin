6334e182d5d9220f8ffed2f50273d8c9a2e0a97c
mayone-du <cocomayo1201@icloud.com>
2021-07-25 22:43:33 +0900

    Add storage username

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@
   $addNewsInput = $doc.querySelector("#jsAddNewsInput");
   $nameInput = $doc.querySelector("#jsNameInput");
   $newsList = $doc.querySelector("#jsNewsList");
+  storageUserNameKey = "QinUserName";
 
   // popupページが読み込まれたら取得して表示 ページのDOMではないので注意
   window.onload = () => {
@@ -12,6 +13,16 @@
     while ($newsList.firstChild) {
       $newsList.removeChild($newsList.firstChild);
     }
+
+    // storageにユーザー名が保存されていたらセット
+    chrome.storage.sync.get(storageUserNameKey, (value) => {
+      // 保存されていなかったら空文字をセット
+      if (value.QinUserName === "" || value.QinUserName === undefined) {
+        $nameInput.value = "";
+        return;
+      }
+      $nameInput.value = value.QinUserName;
+    });
 
     // ニュースのデータを取得
     fetch("https://news-share-backend.herokuapp.com/graphql/", {
@@ -81,21 +92,20 @@
           `;
           newListElement.innerHTML = htmlString;
           $newsList.appendChild(newListElement);
-
-          // ニュースのカウントを表示
-          const newsCount = $newsList.querySelectorAll("li").length.toString();
-          // backgroundへニュースの数を送信
-          chrome.runtime.sendMessage({ newsCount: newsCount }, (response) => {
-            // 受け取ったレスポンスをcontentへ送信
-            console.log(response);
-            //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            //     chrome.tabs.sendMessage(
-            //       tabs[0].id,
-            //       JSON.stringify({ contents: response.newsCount }),
-            //       (response) => {}
-            //     );
-            //   });
-          });
+        });
+        // ニュースのカウントを表示
+        const newsCount = $newsList.querySelectorAll("li").length.toString();
+        // backgroundへニュースの数を送信
+        chrome.runtime.sendMessage({ newsCount: newsCount }, (response) => {
+          // 受け取ったレスポンスをcontentへ送信
+          console.log(response);
+          //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          //     chrome.tabs.sendMessage(
+          //       tabs[0].id,
+          //       JSON.stringify({ contents: response.newsCount }),
+          //       (response) => {}
+          //     );
+          //   });
         });
       });
   };
@@ -103,6 +113,12 @@
   // newsの追加
   $addNewsButton.addEventListener("click", () => {
     try {
+      // TODO:ユーザー名が変更されていた場合のみ保存
+      // ユーザー名の保存
+      chrome.storage.sync.set({ QinUserName: $nameInput.value }, () => {
+        console.log("ユーザー名を保存しました。");
+      });
+
       const newsUrl = $addNewsInput.value;
       // URLのチェック
       if (
@@ -158,10 +174,14 @@
         .then((json) => {
           // 要素自体の生成
           console.log(json);
-          const news = json.data.createNews.news;
-          // 要素自体の生成
-          const newListElement = $doc.createElement("li");
-          const htmlString = `,
+          try {
+            if (json.data.createNews === null) {
+              throw new Error("既に登録済みの可能性があります。");
+            }
+            const news = json.data.createNews.news;
+            // 要素自体の生成
+            const newListElement = $doc.createElement("li");
+            const htmlString = `,
             <a href="${news.url}" rel="noopener noreferrer" target="_blank">
               <span>
                 ${news.title}
@@ -171,9 +191,12 @@
               </span>
             </a>
           `;
-          newLitEslement.innerHTML = htmlString;
-          $newsList.appendChild(newListElement);
-          $addNewsInput.value = "";
+            newListElement.innerHTML = htmlString;
+            $newsList.appendChild(newListElement);
+            $addNewsInput.value = "";
+          } catch (error) {
+            alert(error.message);
+          }
         });
     } catch (error) {
       alert(error);
